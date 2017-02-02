a3536810f9a52d3bdd0fe39d92fda519d553632d
Marcus Christensson <marcus.christensson@gmail.com>
2017-02-02 08:35:36 +0100

    Try load debug build if no release build

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = require('./build/Release/cppmap').WeakValueMap;
+const fs = require("fs");
+
+if (fs.existsSync('./build/Release/cppmap.node')) {
+    module.exports = require('./build/Release/cppmap').WeakValueMap;
+} else if (fs.existsSync('./build/Debug/cppmap.node')) {
+    console.log("weak-value-map loaded debug build");
+    module.exports = require('./build/Debug/cppmap').WeakValueMap;
+} else {
+    console.error("weak-value-map not built!");
+    process.exit(1);
+}
