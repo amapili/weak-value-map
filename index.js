amapili/weak-value-map

commit a3536810f9a52d3bdd0fe39d92fda519d553632d

Try load debug build if no release build

index.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = require('./build/Release/cppmap').WeakValueMap;
+const fs = require("fs");
+
+if (fs.existsSync('./build/Release/cppmap.node')) {
+    module.exports = require('./build/Release/cppmap').WeakValueMap;
+} else if (fs.existsSync('./build/Debug/cppmap.node')) {
+    console.log("weak-value-map loaded debug build");
+    module.exports = require('./build/Debug/cppmap').WeakValueMap;
+} else {
+    console.error("weak-value-map not built!");
+    process.exit(1);
+}

commit cfa9cd5e6208ee22483f8d93563c715e9bca8ad4

Fix bug introduced in a353681

index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
-if (fs.existsSync('./build/Release/cppmap.node')) {
+if (fs.existsSync(path.join(__dirname, 'build/Release/cppmap.node'))) {
     module.exports = require('./build/Release/cppmap').WeakValueMap;
-} else if (fs.existsSync('./build/Debug/cppmap.node')) {
+} else if (fs.existsSync(path.join(__dirname, 'build/Debug/cppmap.node'))) {
     console.log("weak-value-map loaded debug build");
     module.exports = require('./build/Debug/cppmap').WeakValueMap;
 } else {
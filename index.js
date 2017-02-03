cfa9cd5e6208ee22483f8d93563c715e9bca8ad4
Marcus Christensson <marcus.christensson@gmail.com>
2017-02-03 08:15:04 +0100

    Fix bug introduced in a353681

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
-if (fs.existsSync('./build/Release/cppmap.node')) {
+if (fs.existsSync(path.join(__dirname, 'build/Release/cppmap.node'))) {
     module.exports = require('./build/Release/cppmap').WeakValueMap;
-} else if (fs.existsSync('./build/Debug/cppmap.node')) {
+} else if (fs.existsSync(path.join(__dirname, 'build/Debug/cppmap.node'))) {
     console.log("weak-value-map loaded debug build");
     module.exports = require('./build/Debug/cppmap').WeakValueMap;
 } else {
