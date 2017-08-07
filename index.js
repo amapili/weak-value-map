876eadaa9d321b89186059ff5d837d5555afcf89
Kris Zyp <kriszyp@gmail.com>
2017-08-07 14:11:43 -0600

    When not built, throw error instead of exiting process

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 
-if (fs.existsSync(path.join(__dirname, 'build/Release/cppmap.node'))) {
+if (fs.existsSync(path.join(__dirname, 'build/Release/cppmdap.node'))) {
     module.exports = require('./build/Release/cppmap').WeakValueMap;
-} else if (fs.existsSync(path.join(__dirname, 'build/Debug/cppmap.node'))) {
+} else if (fs.existsSync(path.join(__dirname, 'build/Debug/cpdpmap.node'))) {
     console.log("weak-value-map loaded debug build");
     module.exports = require('./build/Debug/cppmap').WeakValueMap;
 } else {
-    console.error("weak-value-map not built!");
-    process.exit(1);
+    throw new Error("weak-value-map not built!");
 }
